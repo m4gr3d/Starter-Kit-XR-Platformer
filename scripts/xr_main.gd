e032c26bd6897a2302e1d680e0e9e8e81de4f658
Fredia Huya-Kouadio <fhuyakou@gmail.com>
2026-09-09 08:19:44 -0700

    Update to the latest spatial container implementation (Godot 4.8 dev5)

--- scripts/xr_main.gd
+++ scripts/xr_main.gd
@@ -6,20 +6,20 @@ const DIMENSION_SCALE_RATIO = 15
 func _ready() -> void:
 	var xr_interface = XRServer.find_interface('OpenXR')
 	if xr_interface == null or not xr_interface.is_initialized():
-		printerr("FHK - Unable to access xr interface...")
+		printerr("Unable to access xr interface...")
 		return
 	
-	var volume_ext = OpenXRSpatialContainerExtension
-	if volume_ext:
-		volume_ext.spatial_container_bounds_changed.connect(_on_volume_bounds_changed)
+	var spatial_container_ext = OpenXRSpatialContainerExtension
+	if spatial_container_ext:
+		spatial_container_ext.spatial_container_bounds_changed.connect(_on_spatial_container_bounds_changed)
 		
-		var volume_bounds = volume_ext.get_spatial_container_bounds()
+		var volume_bounds = spatial_container_ext.get_spatial_container_bounds()
 		_update_scale(volume_bounds)
 	else:
-		printerr("FHK - Unable to access volume extension.")
+		printerr("Unable to access volume extension.")
 
-func _on_volume_bounds_changed(_volume_rid: RID, _volume_infinite_bounds: bool, updated_bounds: Vector3):
-	print("FHK - On volume bounds changed...")
+func _on_spatial_container_bounds_changed(_spatial_container_rid: RID, _infinite_bounds: bool, _bounds_mode: OpenXRSpatialContainerState.BoundsMode, updated_bounds: Vector3):
+	print("Spatial container bounds changed...")
 	_update_scale(updated_bounds)
 
 func _update_scale(bounds: Vector3):
@@ -27,4 +27,4 @@ func _update_scale(bounds: Vector3):
 	# a constant aspect ratio within the spatial container bounds
 	var min_dimension: float = min(bounds.x, min(bounds.y, bounds.z))
 	xr_origin.world_scale = DIMENSION_SCALE_RATIO / min_dimension
-	print("FHK - Updated world_scale to ", xr_origin.world_scale)
+	print("Updated world_scale to ", xr_origin.world_scale)
